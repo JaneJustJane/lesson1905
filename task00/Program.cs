cf05bf794dcc0ee88cdcbed176ecc1cd3ae3eabd
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-22 18:10:26 +0300

    array -> ( - array)

---
+++ task00/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// написать программу замены эл-тов массива на противоположные
+
+int [] array = new int[5]{5,3,2,6,9};
+PrintArray(array);
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    Console.Write($"  {array[i]} ");
+}
+Console.WriteLine();
+ChangeArray(array);
+void ChangeArray(int [] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    array[j]= -array[j];
+
+}
+PrintArray(array);
+
