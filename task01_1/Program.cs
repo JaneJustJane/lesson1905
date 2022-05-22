5fefbba6f7bf53bc7d3ca66cd6346b0619b9f706
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-22 21:08:05 +0300

    if there's a number in your array

---
+++ task01_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Определить, есть ли в массиве число 
+
+int [] array = new int [8]{1,4,5,6,3,2,3,5};
+Console.WriteLine ("введите число ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+void PrintArray(int [] array)
+{
+    for (int i=0; i<array.Length; i++)
+    Console.Write($" {array[i]} ");
+}
+PrintArray (array);
+Console.WriteLine();
+
+void search3(int[] array)
+{
+    for (int j=0; j < array.Length; j++)
+    {
+        if (array[j]==a)
+        Console.WriteLine($"Число {a} - элемент массива под индексом {j}  ");
+        if (array[j]!=a)
+        Console.WriteLine($"Число {array[j]} - не {a}  ");
+    }
+}
+search3(array);
