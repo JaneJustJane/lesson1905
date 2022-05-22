bbf936ce87f0808b9cf3840d712c9b8d43e0241b
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-22 20:04:04 +0300

    search 3 in array

---
+++ task01/Program.cs
@@ -0,0 +1,26 @@
+﻿// Определить, есть ли в массиве число 3
+
+int [] array = new int [8]{2,3,5,6,1,2,8,0};
+void PrintArray(int [] array)
+{
+    for (int i=0; i<array.Length; i++)
+    Console.Write($" {array[i]} ");
+}
+PrintArray (array);
+Console.WriteLine();
+void search3(int[] array)
+{
+    for (int j=0; j < array.Length; j++)
+    {
+        if (array[j]==3)
+        Console.Write($"Число 3 - элемент массива под индексом {j}");
+
+    }
+}
+search3(array);
+
+
+
+
+
+
