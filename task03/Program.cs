fe3b020b7786c875bba6485e0260ebec14c51a77
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-23 02:00:18 +0300

    max of array - min of array

---
+++ task03/Program.cs
@@ -0,0 +1,20 @@
+﻿// В массиве вещественных чисел найти разницу между макс и мин элементом
+
+double[] array = new double [10]{2.6,13,110.9,1,4,5,6,7,8,9};
+void PrintArray(double [] array)
+{
+for (int i=0; i<array.Length; i++)
+Console.Write($" {array[i]} ");
+}
+PrintArray (array); 
+Console.WriteLine();
+//
+double max=array[0];
+double min=array[0];
+for (int j=0; j<array.Length; j++)
+{ 
+    if (array[j]>max)  max = array[j];
+    if (array[j]<min)  min = array[j];
+}
+double result = max - min;
+Console.WriteLine ($"разница между макс и мин эл-ми массива = {result}");
